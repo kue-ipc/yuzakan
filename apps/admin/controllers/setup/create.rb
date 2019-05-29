fbaeda3f64f045f612c4ed52bbafd1e0d82da0cd
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2019-05-29 13:30:33 +0900

    これで問題ないはず

--- apps/admin/controllers/setup/create.rb
+++ apps/admin/controllers/setup/create.rb
@@ -5,9 +5,9 @@ module Admin
         include Admin::Action
 
         def call(params)
-          if params[:admin_user][:password] != params[:admin_user][:current_password]
-            redirect_to routes.setup_path
-          end
+          # if params[:admin_user][:password] != params[:admin_user][:current_password]
+          #   redirect_to routes.setup_path
+          # end
         end
       end
     end
