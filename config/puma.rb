007afd2a40bc80ace09f6356b6cf4a65ea22a8ba
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2020-07-06 11:58:40 +0900

    puma.rbを追加

---
+++ config/puma.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+hanami_env = ENV.fetch('HANAMI_ENV') { 'development' }
+app_root = Dir.pwd
+
+if !ENV.include?('PORT') && hanami_env == 'production'
+  bind 'unix://' + File.expand_path('tmp/sockets/puma.sock', app_root)
+else
+  port ENV.fetch('PORT') { 2300 }
+end
+
+environment hanami_env
+
+if hanami_env == 'production'
+  pidfile File.expand_path('tmp/pids/puma.pid', app_root)
+  stdout_redirect(File.expand_path('log/puma.log', app_root),
+                  File.expand_path('log/puma-error.log', app_root),
+                  true)
+  daemonize true
+end
