c2cb671ee538670f67c173deec55ac778879a9a8
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2020-07-08 15:38:59 +0900

    色を調整？

--- apps/web/controllers/gsuite/show.rb
+++ apps/web/controllers/gsuite/show.rb
@@ -6,11 +6,12 @@ module Web
       class Show
         include Web::Action
 
+        expose :gsuite_provider
         expose :gsuite_user
 
         def call(_params)
-          gsuite_repository = ProviderRepository.new.first_gsuite_with_params
-          @gsuite_user = gsuite_repository.adapter.read(current_user.name)
+          @gsuite_provider = ProviderRepository.new.first_gsuite_with_params
+          @gsuite_user = @gsuite_provider.adapter.read(current_user.name)
         end
       end
     end
