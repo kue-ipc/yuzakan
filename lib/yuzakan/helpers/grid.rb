8e35e26fcf30abcede1c3b44b86f6b82364641cb
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2020-07-15 10:16:46 +0900

    状態ごとのメッセージ表示を作成

--- lib/yuzakan/helpers/grid.rb
+++ lib/yuzakan/helpers/grid.rb
@@ -4,10 +4,28 @@ module Yuzakan
   module Helpers
     module Grid
       private def col_card
-        [
-          'col-sm-6',
-          'col-lg-4',
-          'col-xl-3',
+        %w[
+          col-sm-6
+          col-lg-4
+          col-xl-3
+        ]
+      end
+
+      private def col_name
+        %w[
+          col-sm-6
+          col-md-4
+          col-lg-3
+          col-xl-2
+        ]
+      end
+
+      private def col_value
+        %w[
+          col-sm-6
+          col-md-8
+          col-lg-6
+          col-xl-4
         ]
       end
     end
