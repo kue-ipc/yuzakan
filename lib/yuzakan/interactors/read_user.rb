e11d5f102614fe4da3ab5d2a891d7275b4339850
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2021-06-29 15:58:25 +0900

    パスワードリセットの前に登録されるようにする必要がある。

---
+++ lib/yuzakan/interactors/read_user.rb
@@ -0,0 +1,68 @@
+# TODO 未テスト、作りかけ
+
+class ReadUser
+  include Hanami::Interactor
+
+  class Validations
+    include Hanami::Validations
+    messages_path 'config/messages.yml'
+
+    validations do
+      required(:username) { filled? & str? & size?(1..255) }
+    end
+  end
+
+  def initialize(user:, client:,
+                 user_repository: UserRepository.new,
+                 provider_repository: ProviderRepository.new,
+                 activity_repository: ActivityRepository.new)
+    @user = user
+    @client = client
+    @user_repository = user_repository
+    @provider_repository = provider_repository
+    @activity_repository = activity_repository
+  end
+
+  def call(params)
+    activity_params = {
+      user_id: @user.id,
+      client: @client,
+      type: 'user',
+      target: params[:username],
+      action: 'read_user',
+    }
+
+    user_data = nil
+
+    @provider_repository.operational_all_with_params(:read).each do |provider|
+      user_data = provider.adapter.read(params[:username])
+      # 最初に読み込みされたところを正とする。
+      break if user_data
+    rescue => e
+      @activity_repository.create(**activity_params, result: 'error')
+      error!("ユーザー情報の読み込み時にエラーが発生しました。: #{e.message}")
+    end
+
+    unless user_data
+      @activity_repository.create(**activity_params, result: 'failure')
+      error!('該当のユーザーが見つかりません。')
+    end
+
+    register_user = RegisterUser.new(user: @user, client: @client,
+      user_repository: @user_repository,
+      activity_repository: @activity_repository)
+    create_or_upadte_user(user_data)
+    register_user.call(user_data)
+    @activity_repository.create(**activity_params, result: 'success')
+  end
+
+  private def valid?(params)
+    validation = Validations.new(params).validate
+    if validation.failure?
+      error(validation.messages)
+      return false
+    end
+
+    true
+  end
+end
---
+++ lib/yuzakan/interactors/register_user.rb
@@ -0,0 +1,65 @@
+# TODO 未テスト、作りかけ
+
+class RegisterUser
+  include Hanami::Interactor
+
+  class Validations
+    include Hanami::Validations
+    messages_path 'config/messages.yml'
+
+    validations do
+      required(:name) { filled? & str? & size?(1..255) }
+      optional(:display) { filled? & str & max_size?(255)}
+      optional(:email) { filled? & str & max_size?(255)}
+    end
+  end
+
+  def initialize(user:,
+                 client:,
+                 user_repository: UserRepository.new,
+                 activity_repository: ActivityRepository.new)
+    @user = user
+    @client = client
+    @user_repository = user_repository
+    @activity_repository = activity_repository
+  end
+
+  def call(params)
+    activity_params = {
+      user_id: @user.id,
+      client: @client,
+      type: 'user',
+      target: params[:username],
+      action: 'register_user',
+    }
+
+    name = user_data[:name]
+    display_name = user_data[:display_name] || user_data[:name]
+    email = user_data[:email]
+
+    user = @user_repository.by_name(name).one
+    @user_id =
+      if user.nil?
+        @user_repository.create(name: name,
+                                display_name: display_name,
+                                email: email)
+      elsif user.display_name != display_name || user.email != email
+        @user_repository.update(user.id,
+                                display_name: display_name,
+                                email: email)
+      else
+        user
+      end.id
+    @activity_repository.create(**activity_params, result: 'success')
+  end
+
+  private def valid?(params)
+    validation = Validations.new(params).validate
+    if validation.failure?
+      error(validation.messages)
+      return false
+    end
+
+    true
+  end
+end
