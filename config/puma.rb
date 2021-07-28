71ac4e3b79ed97d9e40e3b0e252657de5bc35e18
KUE IPC <ipc@kyokyo-u.ac.jp>
2021-07-28 13:49:48 +0900

    tmpの中でない場合は作成

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,9 @@ hanami_env = ENV.fetch('HANAMI_ENV') { 'development' }
 app_root = Dir.pwd
 
 if !ENV.include?('PORT') && hanami_env == 'production'
-  bind "unix://#{File.expand_path('tmp/sockets/puma.sock', app_root)}"
+  sockets_dir = File.expand_path('tmp/sockets', app_root)
+  Dir.mkdir(sockets_dir) unless FileTest.directory?(sockets_dir)
+  bind "unix://#{File.join(sockets_dir, 'puma.sock')}"
 else
   port ENV.fetch('PORT', 2300)
 end
@@ -10,7 +12,9 @@ end
 environment hanami_env
 
 if hanami_env == 'production'
-  pidfile File.expand_path('tmp/pids/puma.pid', app_root)
+  pids_dir = File.expand_path('tmp/pids', app_root)
+  Dir.mkdir(pids_dir) unless FileTest.directory?(pids_dir)
+  pidfile File.join(pids_dir, 'puma.pid')
   stdout_redirect(File.expand_path('log/puma.log', app_root),
                   File.expand_path('log/puma-error.log', app_root),
                   true)
