018a8222026e65b59ffae8036cee3cc1f95bb5e1
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2022-01-20 16:35:24 +0900

    パスの深さ違いでのエラーを修正

--- spec/admin/controllers/adapters/param_types/index_spec.rb
+++ spec/admin/controllers/adapters/param_types/index_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../../../spec_helper'
+require_relative '../../../../spec_helper'
 
 describe Admin::Controllers::Adapters::ParamTypes::Index do
   let(:action) { Admin::Controllers::Adapters::ParamTypes::Index.new }
