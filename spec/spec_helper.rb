9d9fe25a86e6d425cabdf0070d220f3395dc1906
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2022-01-21 16:41:47 +0900

    警告を出ないようにする

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ ENV['HANAMI_ENV'] ||= 'test'
 
 require_relative '../config/environment'
 require 'minitest/autorun'
+# minitest/autorun set deprected warning enabled
+# so, set no deprectaed warning
+Warning[:deprecated] = false
 
 require_relative 'support/db'
 
