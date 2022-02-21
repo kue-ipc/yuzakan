15d9fab2fc38c82219b13b8854f8243357a4794e
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2022-02-22 08:45:25 +0900

    showでログインしていない状態を追加

--- spec/api/controllers/current_user/show_spec.rb
+++ spec/api/controllers/current_user/show_spec.rb
@@ -59,4 +59,49 @@ describe Api::Controllers::CurrentUser::Show do
       providers: ['provider'],
     })
   end
+
+  describe 'no login session' do
+    let(:session) { {uuid: uuid} }
+
+    it 'is error' do
+      response = action.call(params)
+      _(response[0]).must_equal 401
+      _(response[1]['Content-Type']).must_equal "#{format}; charset=utf-8"
+      json = JSON.parse(response[2].first, symbolize_names: true)
+      _(json).must_equal({
+        code: 401,
+        message: 'ログインしてください。',
+      })
+    end
+  end
+
+  describe 'no session' do
+    let(:session) { {} }
+
+    it 'is error' do
+      response = action.call(params)
+      _(response[0]).must_equal 401
+      _(response[1]['Content-Type']).must_equal "#{format}; charset=utf-8"
+      json = JSON.parse(response[2].first, symbolize_names: true)
+      _(json).must_equal({
+        code: 401,
+        message: 'ログインしてください。',
+      })
+    end
+  end
+
+  describe 'session timeout' do
+    let(:session) { {uuid: uuid, user_id: user.id, created_at: Time.now - 7200, updated_at: Time.now - 7200} }
+
+    it 'is error' do
+      response = action.call(params)
+      _(response[0]).must_equal 400
+      _(response[1]['Content-Type']).must_equal "#{format}; charset=utf-8"
+      json = JSON.parse(response[2].first, symbolize_names: true)
+      _(json).must_equal({
+        code: 400,
+        message: 'セッションがタイムアウトしました。',
+      })
+    end
+  end
 end
