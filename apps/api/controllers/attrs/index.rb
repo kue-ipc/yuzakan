8367a5d52e2aa502f5b72981e95bda6fa941e4c8
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2022-03-04 12:15:43 +0900

    indexも完成

--- apps/api/controllers/attrs/index.rb
+++ apps/api/controllers/attrs/index.rb
@@ -4,8 +4,16 @@ module Api
       class Index
         include Api::Action
 
-        def call(params)
-          self.body = 'OK'
+        def initialize(attr_repository: AttrRepository.new, **opts)
+          super(**opts)
+          @attr_repository = attr_repository
+        end
+
+        def call(params) # rubocop:disable Lint/UnusedMethodArgument
+          @attrs = @attr_repository.ordered_all
+
+          self.status = 200
+          self.body = generate_json(@attrs)
         end
       end
     end
--- spec/api/controllers/attrs/index_spec.rb
+++ spec/api/controllers/attrs/index_spec.rb
@@ -8,6 +8,7 @@ describe Api::Controllers::Attrs::Index do
                                        attr_repository: attr_repository)
   }
   let(:params) { env }
+
   let(:env) { {'REMOTE_ADDR' => client, 'rack.session' => session, 'HTTP_ACCEPT' => format} }
   let(:client) { '192.0.2.1' }
   let(:uuid) { 'ffffffff-ffff-4fff-bfff-ffffffffffff' }
@@ -15,34 +16,31 @@ describe Api::Controllers::Attrs::Index do
   let(:session) { {uuid: uuid, user_id: user.id, created_at: Time.now - 600, updated_at: Time.now - 60} }
   let(:format) { 'application/json' }
   let(:config) { Config.new(title: 'title', session_timeout: 3600, user_networks: '') }
-  let(:activity_log_repository) { create_mock(create: [nil, [Hash]]) }
-  let(:config_repository) { create_mock(current: config) }
-  let(:user_repository) { create_mock(find: [user, [Integer]]) }
+  let(:activity_log_repository) { ActivityLogRepository.new.tap { |obj| stub(obj).create } }
+  let(:config_repository) { ConfigRepository.new.tap { |obj| stub(obj).current { config } } }
+  let(:user_repository) { UserRepository.new.tap { |obj| stub(obj).find { user } } }
 
-  let(:attr_params) { {name: 'name', label: '表示名', type: 'string'} }
-
-  let(:time) { Time.now - 3600 }
-  let(:all_attr) {
+  let(:all_attrs_attributes) {
     [
-      Attr.new(id: 42, name: 'name42', label: 'ラベル42', type: 'string', order: 1, hidden: false),
-      Attr.new(id: 24, name: 'name24', label: 'ラベル24', type: 'string', order: 2, hidden: false),
-      Attr.new(id: 19, name: 'name19', label: 'ラベル19', type: 'string', order: 3, hidden: false),
-      Attr.new(id: 27, name: 'name27', label: 'ラベル27', type: 'string', order: 4, hidden: false),
+      {id: 42, name: 'name42', label: '表示名42', type: 'string', order: 1, hidden: false},
+      {id: 24, name: 'name24', label: '表示名24', type: 'string', order: 2, hidden: false},
+      {id: 19, name: 'name19', label: '表示名19', type: 'string', order: 3, hidden: false},
+      {id: 27, name: 'name27', label: '表示名27', type: 'string', order: 4, hidden: false},
     ]
   }
-  let(:attr_repository) { create_mock(ordered_all: all_attr) }
+  let(:all_attrs) { all_attrs_attributes.map { |attributes| Attr.new(attributes) } }
+  let(:attr_repository) {
+    AttrRepository.new.tap do |obj|
+      stub(obj).ordered_all { all_attrs }
+    end
+  }
 
   it 'is successful' do
     response = action.call(params)
     _(response[0]).must_equal 200
     _(response[1]['Content-Type']).must_equal "#{format}; charset=utf-8"
     json = JSON.parse(response[2].first, symbolize_names: true)
-    _(json).must_equal [
-      {id: 42, name: 'name42', label: '表示名42', type: 'string', order: 1, hidden: false},
-      {id: 24, name: 'name24', label: '表示名24', type: 'string', order: 2, hidden: false},
-      {id: 19, name: 'name19', label: '表示名19', type: 'string', order: 3, hidden: false},
-      {id: 27, name: 'name27', label: '表示名27', type: 'string', order: 4, hidden: false},
-    ]
+    _(json).must_equal all_attrs_attributes
   end
 
   describe 'no login session' do
