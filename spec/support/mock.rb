c8d5f90b293335d8984dd3bf5adc34faa1b2c709
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2022-03-10 15:14:54 +0900

    attr_mappingsが入るようにする。

--- spec/support/mock.rb
+++ spec/support/mock.rb
@@ -1,4 +1,4 @@
-def create_mock_provider(params: {}, **attributes)
+def create_mock_provider(params: {}, attr_mappings: [], **attributes)
   provider_params = params.map { |key, value| {name: key, value: Marshal.dump(value)} }
-  Provider.new(**attributes, adapter_name: 'mock', provider_params: provider_params)
+  Provider.new(**attributes, adapter_name: 'mock', provider_params: provider_params, attr_mappings: attr_mappings)
 end
