6164f0bfa94b4fdd14cb39f97690ce9d4ce21439
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2022-03-29 10:49:44 +0900

    attrs関係のspecがおかしかったので

--- spec/admin/views/attrs/index_spec.rb
+++ spec/admin/views/attrs/index_spec.rb
@@ -1,9 +1,9 @@
 require_relative '../../../spec_helper'
 
 describe Admin::Views::Attrs::Index do
-  let(:exposures) { {format: :html} }
+  let(:exposures) { Hash[format: :html] }
   let(:template)  { Hanami::View::Template.new('apps/admin/templates/attrs/index.html.slim') }
-  let(:view)      { Admin::Views::Attrs::Index.new(template, **exposures) }
+  let(:view)      { Admin::Views::Attrs::Index.new(template, exposures) }
   let(:rendered)  { view.render }
 
   it 'exposes #format' do
