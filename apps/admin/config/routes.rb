d72fadaee8c81e6298ada080dbae03c7656412b8
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2022-03-29 10:56:43 +0900

    setup部分は保留

--- apps/admin/config/routes.rb
+++ apps/admin/config/routes.rb
@@ -1,13 +1,10 @@
-# Configure your routes here
-# See: http://hanamirb.org/guides/routing/overview/
-#
-# Example:
-# get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 root to: 'home#index'
 
 get '/setup', to: 'setup#index', as: :setup
 post '/setup', to: 'setup#create'
 get '/setup/done', to: 'setup#done', as: :setup_done
+
+# resource :setup, only: [:show, :new, :create]
 
 resource :config, only: [:edit, :update] do
   member do
