8541ddd7d20a4dac16af847dac587614cfcefc34
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2022-05-25 15:57:00 +0900

    テストは省略

--- spec/admin/controllers/groups/show_spec.rb
+++ spec/admin/controllers/groups/show_spec.rb
@@ -1,11 +1,4 @@
 require_relative '../../../spec_helper'
 
 describe Admin::Controllers::Groups::Show do
-  let(:action) { Admin::Controllers::Groups::Show.new }
-  let(:params) { Hash[] }
-
-  it 'is successful' do
-    response = action.call(params)
-    _(response[0]).must_equal 200
-  end
 end
--- spec/api/controllers/groups/show_spec.rb
+++ spec/api/controllers/groups/show_spec.rb
@@ -1,11 +1,4 @@
 require_relative '../../../spec_helper'
 
 describe Api::Controllers::Groups::Show do
-  let(:action) { Api::Controllers::Groups::Show.new }
-  let(:params) { Hash[] }
-
-  it 'is successful' do
-    response = action.call(params)
-    _(response[0]).must_equal 200
-  end
 end
