63923144f69031cadd851407ef5e9d56503b6905
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-01-18 17:06:17 +0900

    レベルマトリックスのテストを作る

---
+++ spec/support/matrix.rb
@@ -0,0 +1,12 @@
+def level_matrix
+  describe 'level matrix' do
+    6.times do |user_level|
+      6.times do |network_level|
+        yield({
+          user_level: user_level,
+          network_level: network_level,
+        })
+      end
+    end
+  end
+end
