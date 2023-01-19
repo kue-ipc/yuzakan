e2175e0414483b0349c354403df215a45a76a87c
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-01-19 15:16:09 +0900

    ルーティングを変更

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -4,18 +4,18 @@ resources :adapters, only: [:index, :show]
 
 resources :attrs, only: [:index, :show, :create, :update, :destroy]
 
-resource :myself, only: [:show] do
+resource :self, only: [:show] do
   resource :password, only: [:update]
+  resources :providers, only: [:show, :create, :destroy] do
+    resource :password, only: [:create]
+    resource :code, only: [:create]
+    resource :lock, only: [:destroy]
+  end
 end
 
 resources :providers, only: [:index, :show, :create, :update, :destroy] do
   member do
     get :check
-  end
-  resource :myself, only: [:show, :create, :destroy] do
-    resource :password, only: [:create]
-    resource :code, only: [:create]
-    resource :lock, only: [:destroy]
   end
 end
 
