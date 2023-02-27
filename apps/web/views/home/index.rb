506e6f8a7d61baef74c3a292c62eb2afa6dce529
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-02-27 10:15:51 +0900

    メニューの整理

--- apps/web/views/home/index.rb
+++ apps/web/views/home/index.rb
@@ -7,20 +7,21 @@ module Web
         include Web::View
 
         def menu_items
-          [
-            {
-              name: 'パスワード変更',
-              url: routes.path(:edit_user_password),
-              description: 'アカウントのパスワードを変更します。',
-              color: 'primary',
-            },
-            {
-              name: 'Google Workspace',
-              url: routes.path(:google),
-              description: 'Googleアカウントを管理します。',
-              color: 'secondary',
-            },
-          ]
+          []
+          # [
+          #   {
+          #     name: 'パスワード変更',
+          #     url: routes.path(:edit_user_password),
+          #     description: 'アカウントのパスワードを変更します。',
+          #     color: 'primary',
+          #   },
+          #   {
+          #     name: 'Google Workspace',
+          #     url: routes.path(:google),
+          #     description: 'Googleアカウントを管理します。',
+          #     color: 'secondary',
+          #   },
+          # ]
         end
       end
     end
