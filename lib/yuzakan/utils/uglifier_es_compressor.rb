0c35c84e62ae07a5b1906bd89e2e3ffa65728bda
KUE IPC <ipc@kyokyo-u.ac.jp>
2023-03-08 11:51:00 +0900

    importが文字列に囲まれている場合に失敗する事がるのを回避

--- lib/yuzakan/utils/uglifier_es_compressor.rb
+++ lib/yuzakan/utils/uglifier_es_compressor.rb
@@ -23,12 +23,12 @@ module Yuzakan
       def modify(data)
         data = data.dup
         [
-          '(\b(?:im|ex)port\b[\s\w,*{}]*\bfrom\b\s*)"([^"]*)"',
-          '(\bimport\b\s*)"([^"]*)"',
-          '(\bimport\b\s*\(\s*)"([^"]*)"(\s*\))',
-        ].each do |re_str|
-          data.gsub!(Regexp.compile(re_str), "\\1\"\\2?v=#{Yuzakan.version}\"\\3")
-          data.gsub!(Regexp.compile(re_str.tr('"', "'")), "\\1'\\2?v=#{Yuzakan.version}'\\3")
+          /(\b(?:im|ex)port\b[\s\w,*{}]*\bfrom\b\s*)"([^"]*)"/,
+          /(?<!")(\bimport\b\s*)"([^"]*)"/,
+          /(\bimport\b\s*\(\s*)"([^"]*)"(\s*\))/,
+        ].each do |re|
+          data.gsub!(re, "\\1\"\\2?v=#{Yuzakan.version}\"\\3")
+          data.gsub!(Regexp.compile(re.source.tr('"', "'")), "\\1'\\2?v=#{Yuzakan.version}'\\3")
         end
         data
       end
