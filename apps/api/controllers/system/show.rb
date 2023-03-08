c8f9692b1d55fbdd0939b78eebe003aebdc89d0f
KUE IPC <ipc@kyokyo-u.ac.jp>
2023-03-09 08:34:19 +0900

    SITE_URLはなくす

--- apps/api/controllers/system/show.rb
+++ apps/api/controllers/system/show.rb
@@ -9,9 +9,8 @@ module Api
         security_level 0
 
         def call(params) # rubocop:disable Lint/UnusedMethodArgument
-          # FIXME: サイトのURLを環境変数からとっている
           self.body = generate_json({
-            url: ENV.fetch('SITE_URL', Web.routes.url(:root)),
+            url: Web.routes.url(:root),
             title: current_config&.title,
             domain: current_config&.domain,
             contact: {
