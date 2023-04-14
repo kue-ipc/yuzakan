9c094dd1e5196cf805ee46dbd2f42abafbaf0890
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-04-14 09:59:48 +0900

    assetのマップがうまくできていなかった

--- lib/yuzakan/helpers/importmap_helper.rb
+++ lib/yuzakan/helpers/importmap_helper.rb
@@ -5,24 +5,25 @@ require 'json'
 module Yuzakan
   module Helpers
     module ImportmapHelper
+      ASSETS_MAP = {'~/' => '/assets/'}.freeze
+
       private def importmap
         html.script(type: 'importmap') do
           raw({
-            imports: all_imports,
+            imports: imports_assets,
           }.to_json)
         end
       end
 
-      private def all_imports
-        asset_json = Hanami.root + 'public/assets.json'
-        if Hanami.env == 'production' && asset_json.file?
-          JSON.parse(asset_json.read).to_h do |path, obj|
-            [path.gsub(%r{^/assets/}, '~/'), obj['target']] if path.end_with?('.js')
-          end.compact
+      private def imports_assets
+        assets_json = Hanami.root / 'public/assets.json'
+        if Hanami.env == 'production' && assets_json.readable?
+          assets_map = JSON.parse(assets_json.read)
+            .select { |path, _| path.end_with?('.js') }
+            .to_h { |path, obj| [path.gsub(%r{^/assets/}, '~/'), obj['target']] }
+          ASSETS_MAP.merge(assets_map)
         else
-          {
-            '~/' => '/assets/',
-          }
+          ASSETS_MAP
         end
       end
     end
