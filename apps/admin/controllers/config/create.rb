59f79035852c35d51db2eff281463f465f758d59
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-04-17 13:45:53 +0900

    初期セットアップを整理する

--- apps/admin/controllers/config/create.rb
+++ apps/admin/controllers/config/create.rb
@@ -60,12 +60,13 @@ module Admin
             return
           end
 
-          setup_network &&
-            setup_local_provider &&
-            setup_admin(params[:config][:admin_user].to_h) &&
+          begin
+            setup_network
+            setup_local_provider
+            setup_admin(params[:config][:admin_user].to_h)
             setup_config(params[:config].except(:admin_user).to_h)
-
-          unless flash[:errors].empty?
+          rescue => e
+            flash[:errors] << e.message
             flash[:failure] = '設定に失敗しました。'
             self.body = Admin::Views::Config::New.render(exposures)
             return
@@ -79,7 +80,7 @@ module Admin
         end
 
         private def setup_network
-          return true if @network_repository.count.positive?
+          return if @network_repository.count.positive?
 
           @network_repository.transaction do
             [
@@ -94,12 +95,10 @@ module Admin
               @network_repository.create_or_update_by_address(address, {clearance_level: 1, trusted: false})
             end
           end
-
-          true
         end
 
         private def setup_local_provider
-          return true if @provider_repository.find_by_name('local')
+          return if @provider_repository.find_by_name('local')
 
           @provider_repository.create({
             name: 'local',
@@ -112,55 +111,52 @@ module Admin
             password_changeable: true,
             lockable: true,
           })
-
-          true
         end
 
         private def setup_admin(admin_user_params)
-          # return true if @user_repository.find_by_name(admin_user_params[:username])
-
-          sync_user = SyncUser.new(provider_repository: @provider_repository,
-                                   user_repository: @user_repository,
-                                   group_repository: @group_repository,
-                                   member_repository: @member_repository)
-
-          sync_result = sync_user.call(admin_user_params.slice(:username))
-          if sync_result.failure?
-            sync_result[:errors].concat(sync_result.errors)
-            return false
-          end
-          admin_user = sync_result.user
+          admin_user = get_user(admin_user_params[:username])
 
           unless admin_user
-            create_result = ProviderCreateUser.new(provider_repository: @provider_repository)
-              .call({
-                **admin_user_params.slice(:username, :password),
-                providers: ['local'],
-                display_name: 'ローカル管理者',
-              })
+            create_result = proider_create_user.call({
+              **admin_user_params.slice(:username, :password),
+              providers: ['local'],
+              display_name: 'ローカル管理者',
+            })
             if create_result.failure?
               flash[:errors].concat(create_result.errors)
               return false
             end
 
-            sync_result = sync_user.call(admin_user_params.slice(:username))
-            if sync_result.failure?
-              sync_result[:errors].concat(sync_result.errors)
-              return false
-            end
-            admin_user = sync_result.user
+            admin_user = get_user(admin_user_params[:username])
           end
 
           @user_repository.update(admin_user.id, clearance_level: 5) if admin_user.clearance_level < 5
-
-          true
         end
 
         private def setup_config(config_params)
-          return true if @config_repository.current
+          return if @config_repository.current
 
           @config_repository.current_create({**config_params, maintenace: false})
-          true
+        end
+
+        private def sync_user
+          @sync_user ||= SyncUser.new(provider_repository: @provider_repository,
+                                      user_repository: @user_repository,
+                                      group_repository: @group_repository,
+                                      member_repository: @member_repository)
+        end
+
+        private def proider_create_user
+          @proider_create_user = ProviderCreateUser.new(provider_repository: @provider_repository)
+        end
+
+        private def get_user(username)
+          sync_result = sync_user.call({username: username})
+          if sync_result.failure?
+            sync_result[:errors].concat(sync_result.errors)
+            raise '同期処理に失敗しました'
+          end
+          sync_result.user
         end
       end
     end
