7c40646d331a4c8bcfa6591d7fb7f5b9a004a69a
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-04-17 14:30:43 +0900

    メソッドが長くならないように整理

--- apps/admin/controllers/config/replace.rb
+++ apps/admin/controllers/config/replace.rb
@@ -80,65 +80,60 @@ module Admin
           redirect_to routes.path(:edit_config)
         rescue => e
           Hanami.logger.error e
+          flash[:failure] = 'インポートに失敗しました。'
           flash[:errors] << e.message
-          flash[:error] = 'エラーが発生しました。'
           self.body = Admin::Views::Config::Edit.render(exposures)
         end
 
         def import_yaml(upload_file)
-          begin
-            yaml = upload_file[:tempfile].read
-            data = YAML.safe_load(yaml, permitted_classes: [Symbol, Time, Date],
-                                        filename: upload_file[:filename],
-                                        symbolize_names: true)
-          rescue Psych::SyntaxError => e
-            flash[:errors] << 'YAMLファイルの形式が不正です。'
-            flash[:errors] << e.message
-            return
-          end
+          data = yaml_load(upload_file[:tempfile].read, filename: upload_file[:filename])
 
+          data = validate_data(data)
+
+          update_all(data)
+        end
+
+        private def yaml_load(yaml, filename: nil)
+          YAML.safe_load(yaml, permitted_classes: [Symbol, Time, Date],
+                               filename: filename,
+                               symbolize_names: true)
+        end
+
+        private def validate_data(data)
           validate_result = YamlValidator.new(data).validate
           if validate_result.failure?
-            flash[:errors] << 'インポートするファイルのパラメーターが不正です。'
             flash[:errors] << {file: validate_result.messages}
-            return
+            raise 'インポートするファイルのパラメーターが不正です。'
           end
-          data = validate_result.output
+          validate_result.output
+        end
 
-          begin
-            @config_repository.transaction do
-              update_config(data[:config]) if data[:config]
-              raise '全体設定の設定に失敗しました。' unless flash[:errors].empty?
+        private def update_all(data)
+          @config_repository.transaction do
+            update_config(data[:config]) if data[:config]
 
-              update_providers(data[:providers]) if data[:providers]
-              raise 'プロバイダーの設定に失敗しました。' unless flash[:errors].empty?
+            update_providers(data[:providers])
 
-              update_attrs(data[:attrs]) if data[:attrs]
-              raise '属性の設定に失敗しました。' unless flash[:errors].empty?
-            end
-          rescue => e
-            Hanami.logger.error e
-            flash[:errors] << e.message
-            flash[:errors] << 'インポートに失敗しました。'
+            update_attrs(data[:attrs]) if data[:attrs]
           end
         end
 
         def update_config(config_data)
           @config_repository.current_update(config_data)
+        rescue
+          flash[:errors] << '全体設定の設定に失敗しました。'
+          raise
         end
 
         def update_providers(provider_datas)
-          existing_providers = named_providers.dup
-          # プロバイダーのリストは初期化しておく
-          @named_providers = {}
+          existing_providers = @provider_repository.all.to_h { |provider| [provider.name, provider] }
 
           provider_datas.each_with_index do |provider_data, idx|
             provider_name = provider_data[:name]
             current_provider = existing_providers.delete(provider_name)
 
-            data = provider_data.dup
-            provider_params = provider_data.delete(:params)
-            data[:order] = idx * 8
+            data = {**provider_data, order: idx * 8}
+            provider_params = data.delete(:params)
 
             provider =
               if current_provider
@@ -146,51 +141,45 @@ module Admin
               else
                 @provider_repository.create(data)
               end
-            # プロバイダーのリストを作り直す
-            @named_providers[provider.name] = provider
 
-            next unless provider_params
-
-            existing_params = @provider_param_repository.all_by_provider(provider)
-              .to_h { |param| [param.name, param] }
-
-            provider.adapter_param_types.each do |param_type|
-              param_name = param_type.name
-
-              # 存在チェック
-              next unless provider_params.key?(param_name)
-
-              value = param_type.convert_value(provider_params[param_type.name])
-              next if value.nil?
-
-              current_param = existing_params.delete(param_name.to_s)
-
-              param_data = {
-                provider_id: provider.id,
-                name: param_name.to_s,
-                value: param_type.dump_value(value),
-              }
-
-              if current_param
-                @provider_param_repository.update(current_param.id, param_data)
-              else
-                @provider_param_repository.create(param_data)
-              end
-            end
-
-            # リストになかったパラメーターを削除
-            existing_params.each_value do |param|
-              @provider_param_repository.delete(param.id)
-            end
+            update_provider_params(provider, provider_params) if provider_params
           end
 
           # リストになかったプロバイダーを削除
           existing_providers.each_value do |provider|
             @provider_repository.delete(provider.id)
           end
+        rescue
+          flash[:errors] << 'プロバイダーの設定に失敗しました。'
+          raise
+        end
+
+        def update_provider_params(provider, params)
+          existing_params = @provider_param_repository.all_by_provider(provider).to_h { |param| [param.name, param] }
+
+          provider.adapter_param_types.each do |param_type|
+            param_name = param_type.name
+            # 存在チェック
+            next unless params.key?(param_name) && !(value = param_type.convert_value(params[param_type.name])).nil?
+
+            current_param = existing_params.delete(param_name.to_s)
+            param_data = {provider_id: provider.id, name: param_name.to_s,
+                          value: param_type.dump_value(value),}
+            if current_param
+              @provider_param_repository.update(current_param.id, param_data)
+            else
+              @provider_param_repository.create(param_data)
+            end
+          end
+
+          # リストになかったパラメーターを削除
+          existing_params.each_value do |param|
+            @provider_param_repository.delete(param.id)
+          end
         end
 
         def update_attrs(attr_datas)
+          named_providers = @provider_repository.all.to_h { |provider| [provider.name, provider] }
           @attr_repository.clear
 
           attr_datas.each_with_index do |attr_data, idx|
@@ -202,19 +191,14 @@ module Admin
               &.map do |am_params|
                 {
                   **am_params.slice(:key, :conversion),
-                  provider_id: provider_id_by_name(am_params[:provider]).id,
+                  provider_id: named_providers[am_params[:provider]].id,
                 }
               end
             @attr_repository.create_with_mappings(data)
           end
-        end
-
-        private def named_providers
-          @named_providers ||= @provider_repository.all.to_h { |provider| [provider.name, provider] }
-        end
-
-        private def provider_id_by_name(name)
-          named_providers[name]
+        rescue
+          flash[:errors] << '属性の設定に失敗しました。'
+          raise
         end
       end
     end
