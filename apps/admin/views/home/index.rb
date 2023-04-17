d76912d6a6638f6b2b14290c54be6957ab07e5f2
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-04-17 15:29:28 +0900

    メニューの作り方を変える

--- apps/admin/views/home/index.rb
+++ apps/admin/views/home/index.rb
@@ -6,46 +6,66 @@ module Admin
       class Index
         include Admin::View
 
-        def menu_items
+        MENU_ITEMS =
           [
             {
-              name: '全体設定',
-              url: routes.path(:edit_config),
-              description: 'サイト名やパスワードの条件等の設定はこちらから変更できます。',
-              color: 'danger',
-            },
-            {
-              name: 'プロバイダー',
-              url: routes.path(:providers),
-              description: '連携するシステムはプロバイダーとして登録します。連携システムの追加や変更が可能です。',
-              color: 'danger',
-            },
-            {
-              name: 'ユーザー属性情報',
-              url: routes.path(:attrs),
-              description: '各プロバイダーからのユーザー属性の紐付けを行います。',
-              color: 'danger',
-            },
-            {},
-            {
               name: 'ユーザー作成',
-              url: routes.path(:user, '*'),
+              action: :user,
+              action_params: {id: '*'},
               description: 'ユーザーを新規作成します。',
               color: 'primary',
+              level: 4,
             },
             {
               name: 'ユーザー一覧',
-              url: routes.path(:users),
+              action: :users,
               description: 'ユーザーの一覧です。',
               color: 'secondary',
+              level: 2,
             },
             {
               name: 'グループ一覧',
-              url: routes.path(:groups),
+              action: :groups,
               description: 'グループの一覧です。',
               color: 'secondary',
+              level: 2,
             },
-          ]
+            {},
+            {
+              name: '全体設定',
+              action: :edit_config,
+              description: 'サイト名やパスワードの条件等の設定はこちらから変更できます。',
+              color: 'danger',
+              level: 5,
+            },
+            {
+              name: 'プロバイダー',
+              action: :providers,
+              description: '連携するシステムはプロバイダーとして登録します。連携システムの追加や変更が可能です。',
+              color: 'danger',
+              level: 2,
+            },
+            {
+              name: 'ユーザー属性情報',
+              action: :attrs,
+              description: '各プロバイダーからのユーザー属性の紐付けを行います。',
+              color: 'danger',
+              level: 5,
+            },
+          ].freeze
+        def menu_items
+          MENU_ITEMS.map do |menu|
+            next {} if menu.empty?
+            next if menu[:level] > current_level
+
+            pp menu
+            {
+              name: menu[:name],
+              url: routes.path(menu[:action], **menu.fetch(:action_params, {})),
+              description: menu[:description],
+              color: menu[:color],
+            }
+          end.compact
         end
       end
     end
