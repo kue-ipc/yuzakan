ece5c001cbc0ee785a5c5eb39e0648aa8f41317e
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-12-06 14:50:52 +0900

    ローカルのユーザーをjsonlで取得できるようにする

--- apps/admin/config/routes.rb
+++ apps/admin/config/routes.rb
@@ -5,7 +5,11 @@ root to: 'home#index'
 resource :config, only: [:show, :new, :create, :edit, :update]
 put 'config', to: 'config#replace', as: :config
 
-resources :providers, only: [:index, :show]
+resources :providers, only: [:index, :show] do
+  member do
+    get 'export'
+  end
+end
 
 resources :attrs, only: [:index]
 
