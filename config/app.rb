3005b436224ae4f4c58f53506c7d8c555f50180b
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-12-11 15:43:55 +0900

    sessionのシークレット

--- config/app.rb
+++ config/app.rb
@@ -21,7 +21,7 @@ ADAPTERS_MANAGER = Yuzakan::Adapters::Manager.new
 module Yuzakan
   class App < Hanami::App
     config.actions.sessions = :cookie, {
-      key: "bookshelf.session",
+      key: "yuzakan.session",
       secret: settings.session_secret,
       expire_after: 60 * 60 * 24 * 365,
     }
