c8a81b3b0ea5a1af74afe48d879fd6772e27e7d0
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-12-12 10:55:46 +0900

    rubocop -A

--- spec/slices/user/actions/home/index_spec.rb
+++ spec/slices/user/actions/home/index_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe User::Actions::Home::Index do
-  let(:params) { Hash[] }
+  let(:params) { {} }
 
   it "works" do
     response = subject.call(params)
