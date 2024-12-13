7c1c9a86fe22dc356186e236a9444fde85a2ce08
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-12-13 13:31:13 +0900

    ビューのjsでcivet等が使えるようにする

--- app/view.rb
+++ app/view.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require "hanami/view"
+require "slim"
 
 module Yuzakan
   class View < Hanami::View
--- config/assets.js
+++ config/assets.js
@@ -1,16 +1,30 @@
 import * as assets from "hanami-assets";
 
-await assets.run();
+import coffeeScriptPlugin from 'esbuild-coffeescript';
+import civetPlugin from '@danielx/civet/esbuild-plugin';
 
-// To provide additional esbuild (https://esbuild.github.io) options, use the following:
-//
-// Read more at: https://guides.hanamirb.org/assets/customization/
-//
-// await assets.run({
-//   esbuildOptionsFn: (args, esbuildOptions) => {
-//     // Add to esbuildOptions here. Use `args.watch` as a condition for different options for
-//     // compile vs watch.
-//
-//     return esbuildOptions;
-//   }
-// });
+await assets.run({
+  esbuildOptionsFn: (args, esbuildOptions) => {
+    // Add to esbuildOptions here. Use `args.watch` as a condition for different options for
+    // compile vs watch.
+    // if (args.watch) {
+    // } else {
+    // }
+
+    esbuildOptions.format = 'esm';
+    esbuildOptions.target = [
+      'edge128', // for Extended Stable
+      'chrome126', // for ChromeOS LTS 126
+      'firefox128', // for ESR
+      'safari16', // for iOS 16
+    ];
+
+    esbuildOptions.plugins ??= [];
+    esbuildOptions.plugins.push(coffeeScriptPlugin());
+    esbuildOptions.plugins.push(civetPlugin());
+
+    console.log(esbuildOptions);
+
+    return esbuildOptions;
+  }
+});
