98291eec4d47ab75caa50bcb928fae64063cbba7
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-12-20 09:49:56 +0900

    adapterの作成を整理中、キャッシュは除外した。

--- app/providers/adapter.rb
+++ app/providers/adapter.rb
@@ -10,38 +10,22 @@ module Yuzakan
 
       def call(provider)
         provider = step get_provider(provider)
-        adapter_class = step get_adapter_class(provider.adapter)
-
-        # cache_store
-        expires_in = case Hanami.env
-                     when "production" then 60 * 60
-                     when "development" then 60
-                     else 0
-                     end
-        namespace = ["yuzakan", "provider", attributes[:name]].join(":")
-        redis_url = ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/0")
-        @cache_store = Yuzakan::Utils::CacheStore.create_store(
-          expires_in: expires_in, namespace: namespace, redis_url: redis_url)
+        adapter_class = step get_class(provider)
+        step get_params(provider)
+        step create(adapter_class, provider)
 
         provider_params_hash = attributes[:provider_params].to_h do |param|
           [param[:name].intern, param[:value]]
         end
         @params = @adapter_class.normalize_params(provider_params_hash)
         @adapter = @adapter_class.new(@params, group: attributes[:group], logger: Hanami.logger)
-        super
+        Success(adapter)
       end
 
       def get_provider(provider)
         return Failure(:nil) if provider.nil?
 
-        provider =
-          if provider.is_a?(Yuzakan::Structs::Provider)
-            provider_repo.get(provider.to_s)
-          elsif provider.provider_params.nil?
-            provider_repo.get(provider.name)
-          else
-            provider
-          end
+        provider = provider_repo.get(provider.to_s) unless provider.is_a?(Yuzakan::Structs::Provider)
 
         if provider
           Success(provider)
@@ -57,6 +41,15 @@ module Yuzakan
         else
           Failure(:not_found)
         end
+      end
+
+      def create_adatper(_adapter_class, _params)
+        provider_params_hash = attributes[:provider_params].to_h do |param|
+          [param[:name].intern, param[:value]]
+        end
+        @params = @adapter_class.normalize_params(provider_params_hash)
+        @adapter = @adapter_class.new(@params, group: attributes[:group], logger: Hanami.logger)
+        Success(adapter)
       end
     end
   end
