ec06000b69c5a61634e44cd66f26a480263e6c77
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-01-15 16:43:43 +0900

    localスライスはDBのみ残す

--- slices/local/operation.rb
+++
@@ -1,7 +0,0 @@
-# auto_register: false
-# frozen_string_literal: true
-
-module Local
-  class Operation < Yuzakan::Operation
-  end
-end
--- spec/slices/local/action_spec.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Local::Action do
-  xit "works"
-end
