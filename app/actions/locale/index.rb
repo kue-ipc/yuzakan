62215cda198216cfdb2623569f080dcaccc1205e
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-03-04 10:35:33 +0900

    i18nとstatus icon周りができる

--- app/actions/locale/index.rb
+++ app/actions/locale/index.rb
@@ -8,12 +8,7 @@ module Yuzakan
 
         def handle(_request, response)
           response.format = :json
-          translations = I18n.backend.translations[I18n.locale]
-          response.body = {
-            language: I18n.locale,
-            hash: [translations.hash].pack("q<").unpack1("H16"),
-            translations: translations,
-          }.to_json
+          response.body = I18n.backend.translations[I18n.locale].to_json
         end
       end
     end
