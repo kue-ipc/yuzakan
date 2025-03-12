b50e46e6766e029c1c93d82f8efdd13f29f419de
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-03-12 11:08:20 +0900

    databasのバージョンを上げる

--- config/db/structure.sql
+++ config/db/structure.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 13.18
--- Dumped by pg_dump version 13.18
+-- Dumped from database version 13.20
+-- Dumped by pg_dump version 13.20
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
