63e4363585d0023c61ace72d3620702ea1350f6f
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-04-09 15:10:57 +0900

    ログアウトボタンまでできた

--- app/views/parts/config.rb
+++ app/views/parts/config.rb
@@ -5,12 +5,20 @@ module Yuzakan
   module Views
     module Parts
       class Config < Yuzakan::Views::Part
-        def title_tag
-          helpers.tag.title(value.title)
+        def title
+          value.title
         end
 
-        def title_link_tag(url = _context.routes.path(:root), **)
-          helpers.link_to(value.title, url, **)
+        def title_tag
+          helpers.tag.title(title)
+        end
+
+        def title_link_tag(url = context.routes.path(:root), **, &)
+          if block_given?
+            helpers.link_to(url, **, &)
+          else
+            helpers.link_to(title, url, **)
+          end
         end
       end
     end
--- app/views/parts/user.rb
+++ app/views/parts/user.rb
@@ -5,6 +5,21 @@ module Yuzakan
   module Views
     module Parts
       class User < Yuzakan::Views::Part
+        def path
+          context.routes.path(:user_root)
+        end
+
+        def title
+          value.label_name
+        end
+
+        def link_tag(**, &)
+          if block_given?
+            helpers.link_to(path, **, &)
+          else
+            helpers.link_to(title, path, **)
+          end
+        end
       end
     end
   end
