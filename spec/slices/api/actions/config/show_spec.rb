9943ad83f552cb582d77ea317aedba806d5bedac
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-05-13 11:49:13 +0900

    ひとまず、何も考えずにupdateのテストも作る

--- spec/slices/api/actions/config/show_spec.rb
+++ spec/slices/api/actions/config/show_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe API::Actions::Config::Show do
-  let(:params) { {} }
+  init_action_spec
 
-  # it "works" do
-  #   response = subject.call(params)
-  #   expect(response).to be_successful
-  # end
+  it "is successful" do
+    response = action.call(params)
+    expect(response).to be_successful
+  end
 end
--- spec/slices/api/actions/config/update_spec.rb
+++ spec/slices/api/actions/config/update_spec.rb
@@ -1,10 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe API::Actions::Config::Update do
-  let(:params) { {} }
+  init_action_spec
 
-  # it "works" do
-  #   response = subject.call(params)
-  #   expect(response).to be_successful
-  # end
+  it "is failure" do
+    response = action.call(params)
+    expect(response).to be_client_error
+    expect(response.status).to eq 403
+  end
+
+  context "when admin" do
+    let(:user) { create_struct(:user, :superuser) }
+    # let(:user) {
+    #   create_sturct(Yuzakan::Structs::User, Hanami.app["relations.users"],
+    #     Factory.structs[:superuser].attributes)
+    # }
+
+    it "is successful" do
+      response = action.call(params)
+      expect(response).to be_successful
+    end
+  end
 end
