ece56f67e43b84272c3867d43ba379867d339555
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-05-23 11:53:08 +0900

    effectだった

--- app/assets/js/@types/hyperapp__time.d.ts
+++ app/assets/js/@types/hyperapp__time.d.ts
@@ -1,8 +1,7 @@
 declare module '@hyperapp/time' {
-  import type {Action, Subscription} from "hyperapp";
+  import type {Action, Effect, Subscription} from "hyperapp";
 
   const every: <S>(delay: number, action: Action<S, Date>) => Subscription<S>;
-  const delay: <S>(delay: number, action: Action<S, Date>) => Subscription<S>;
-  const now: <S>(action: Action<S, Date>) => Subscription<S>
+  const delay: <S>(delay: number, action: Action<S, Date>) => Effect<S>;
+  const now: <S>(action: Action<S, Date>) => Effect<S>
 }
-
