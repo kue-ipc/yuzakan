25c54b85efe3bcc46238309986b293391bfd2dcd
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-07-01 14:23:20 +0900

    groupを追加

---
+++ slices/api/actions/groups/create.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module API
+  module Actions
+    module Groups
+      class Create < API::Action
+        def handle(request, response)
+        end
+      end
+    end
+  end
+end
--- slices/api/config/routes.rb
+++ slices/api/config/routes.rb
@@ -9,14 +9,42 @@ module API
     get "/config", to: "config.show", as: :config
     patch "/config", to: "config.update", as: :config
 
-    # get "/adapters", to: "adapters.index", as: :adapters
-    # get "/adapters/:id", to: "adapters.show", as: :adapter
+    get "/attrs", to: "attrs.index", as: :attrs
+    post "/attrs", to: "attrs.create", as: :attrs
+    get "/attrs/:id", to: "attrs.show", as: :attr
+    patch "/attrs/:id", to: "attrs.update", as: :attr
+    delete "/attrs/:id", to: "attrs.destroy", as: :attr
 
-    # get "/attrs", to: "attrs.index", as: :attrs
-    # post "/attrs", to: "attrs.create", as: :attrs
-    # get "/attrs/:id", to: "attrs.show", as: :attr
-    # patch "/attrs/:id", to: "attrs.update", as: :attr
-    # delete "/attrs/:id", to: "attrs.destroy", as: :attr
+    get "/groups", to: "groups.index", as: :groups
+    post "/groups", to: "groups.create", as: :groups
+    get "/groups/:id", to: "groups.show", as: :group
+    patch "/groups/:id", to: "groups.update", as: :group
+    delete "/groups/:id", to: "groups.destroy", as: :group
+
+    post "/mfa/email", to: "mfa.email.create", as: :mfa_email
+    delete "/mfa/email", to: "mfa.email.destroy", as: :mfa_email
+    patch "/mfa/email", to: "mfa.email.update", as: :mfa_email
+
+    get "/providers", to: "providers.index", as: :providers
+    post "/providers", to: "providers.create", as: :providers
+    get "/providers/:id", to: "providers.show", as: :provider
+    patch "/providers/:id", to: "providers.update", as: :provider
+    delete "/providers/:id", to: "providers.destroy", as: :provider
+    get "/providers/:id/check", to: "providers.check"
+
+    get "/session", to: "session.show", as: :session
+
+    get "/system", to: "system.show", as: :session
+
+    get "/users", to: "users.index", as: :users
+    post "/users", to: "users.create", as: :users
+    get "/users/:id", to: "users.show", as: :user
+    patch "/users/:id", to: "users.update", as: :user
+    delete "/users/:id", to: "users.destroy", as: :user
+    post "/users/:id/password", to: "users/password.create", as: :user_password
+    patch "/users/:id/password", to: "users.password.update", as: :user_password
+    post "/users/:id/lock", to: "users/lock.create", as: :user_lock
+    delete "/users/:id/lock", to: "users/lock.destroy", as: :user_lock
 
     # TODO: わけずにユーザーの処理にする
     # resource :self, only: [:show] do
@@ -27,38 +55,6 @@ module API
     #     resource :lock, only: [:destroy]
     #   end
     # end
-
-    # get "/providers", to: "providers.index", as: :providers
-    # post "/providers", to: "providers.create", as: :providers
-    # get "/providers/:id", to: "providers.show", as: :provider
-    # patch "/providers/:id", to: "providers.update", as: :provider
-    # delete "/providers/:id", to: "providers.destroy", as: :provider
-    # get "/providers/:id/check", to: "providers.check"
-
-    get "/session", to: "session.show", as: :session
-
-    post "/mfa/email", to: "mfa.email.create", as: :mfa_email
-    delete "/mfa/email", to: "mfa.email.destroy", as: :mfa_email
-    patch "/mfa/email", to: "mfa.email.update", as: :mfa_email
-
-    # get "/users", to: "users.index", as: :users
-    # post "/users", to: "users.create", as: :users
-    # get "/users/:id", to: "users.show", as: :user
-    # patch "/users/:id", to: "users.update", as: :user
-    # delete "/users/:id", to: "users.destroy", as: :user
-    # post "/users/:id/password", to: "users/password.create", as: :user_password
-    patch "/users/:id/password", to: "users.password.update", as: :user_password
-    # post "/users/:id/lock", to: "users/lock.create", as: :user_lock
-    # delete "/users/:id/lock", to: "users/lock.destroy", as: :user_lock
-
-    # get "/groups", to: "groups.index", as: :groups
-    # get "/groups/:id", to: "groups.show", as: :group
-    # patch "/groups/:id", to: "groups.update", as: :group
-    # get "/groups/:id/members", to: "groups/members.index", as: :group_members
-    # patch "/groups/:id/members/:user_id", to: "groups/members.update",
-    #   as: :group_member
-    # delete "/groups/:id/members/:user_id", to: "groups/members.destroy",
-    #   as: :group_member
 
     # get "/system", to: "system.show", as: :system
 
