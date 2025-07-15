7a982985fbcfc0930f958a09c335c4164dc074bf
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-07-15 11:48:58 +0900

    affilaction完成

--- slices/api/views/parts/affiliation.rb
+++ slices/api/views/parts/affiliation.rb
@@ -5,6 +5,11 @@ module API
   module Views
     module Parts
       class Affiliation < API::Views::Part
+        def to_h(simple: false)
+          value.to_h.slice(:name, :label, :note)
+        end
+
+        def to_json(simple: false) = helpers.params_to_json(to_h(simple:))
       end
     end
   end
