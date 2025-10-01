1328e214258d4b5df380230e80c6cc18b7c0bf8e
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-10-01 15:42:40 +0900

    local_をつけるようにする

--- lib/yuzakan/adapters/local.rb
+++ lib/yuzakan/adapters/local.rb
@@ -19,13 +19,13 @@ module Yuzakan
       end
 
       def user_create(username, userdata, password: nil)
-        return if user_repo.exist_by_name?(username)
+        return if local_user_repo.exist_by_name?(username)
 
         hashed_password = hash_password(password)
-        primary_group = userdata.primary_group
-          &.then { |name| group_repo.find_by_name(name) }
-        member_groups = userdata.groups&.difference([userdata.primary_group])
-          &.then { |names| group_repo.all_by_names(names) }
+        local_group = userdata.primary_group
+          &.then { |name| local_group_repo.find_by_name(name) }
+        local_members = userdata.groups&.difference([userdata.primary_group])
+          &.then { |names| local_group_repo.all_by_names(names) }
 
         params = {
           name: username,
@@ -33,43 +33,43 @@ module Yuzakan
           label: userdata.label || "",
           email: userdata.email || "",
           attrs: userdata.attrs || {},
-          local_group_id: primary_group&.id,
-          local_members: member_groups
-            &.map { |group| {local_group_id: group.id} } || [],
+          local_group_id: local_group&.id,
+          local_members: local_members
+            &.map { |local_member| {local_group_id: local_member.id} } || [],
         }
-        user_repo.create_with_members(**params)
+        local_user_repo.create_with_members(**params)
 
         user_read(username)
       end
 
       def user_read(username)
-        user_struct_to_data(user_repo.find_with_groups_by_name(username))
+        user_struct_to_data(local_user_repo.find_with_groups_by_name(username))
       end
 
       def user_update(username, **userdata)
-        user = user_repo.find_by_name(username)
-        return if user.nil?
+        local_user = local_user_repo.find_by_name(username)
+        return if local_user.nil?
 
         primary_group = userdata.primary_group
-          &.then { |name| group_repo.find_by_name(name) }
+          &.then { |name| local_group_repo.find_by_name(name) }
         member_groups = userdata.groups&.difference([userdata.primary_group])
-          &.then { |names| group_repo.all_by_names(names) }
+          &.then { |names| local_group_repo.all_by_names(names) }
 
         params = {
           label: userdata.label,
           email: userdata.email,
-          **{attrs: userdata.attrs&.merge(user.attrs)}.comact,
+          **{attrs: userdata.attrs&.merge(local_user.attrs)}.comact,
           local_group_id: primary_group&.id,
         }
-        user_repo.transaction do
-          user_repo.update(user.id, **params)
+        local_user_repo.transaction do
+          local_user_repo.update(local_user.id, **params)
           if member_groups
             remain_group_ids = member_groups.map(&:id)
-            user_repo.find_with_members(user.id).local_members.each do |member|
-              member_repo.delete(member.id) unless remain_group_ids.delete(member.local_group_id)
+            local_user_repo.find_with_members(local_user.id).local_members.each do |local_member|
+              local_member_repo.delete(local_member.id) unless remain_group_ids.delete(local_member.local_group_id)
             end
             remain_group_ids.each do |group_id|
-              member_repo.create(local_user_id: user.id,
+              local_member_repo.create(local_user_id: local_user.id,
                 local_group_id: group_id)
             end
           end
@@ -79,143 +79,143 @@ module Yuzakan
       end
 
       def user_delete(username)
-        user = user_repo.find_with_groups_by_name(username)
-        return if user.nil?
+        local_user = local_user_repo.find_with_groups_by_name(username)
+        return if local_user.nil?
 
-        user_repo.delete(user.id)
-        user_struct_to_data(user)
+        local_user_repo.delete(local_user.id)
+        user_struct_to_data(local_user)
       end
 
       def user_auth(username, password)
-        user = user_repo.find_by_name(username)
-        return if user.nil?
-        return false if user.locked
-        return false if user.hashed_password.empty?
+        local_user = local_user_repo.find_by_name(username)
+        return if local_user.nil?
+        return false if local_user.locked
+        return false if local_user.hashed_password.empty?
 
-        verify_password(password, user.hashed_password)
+        verify_password(password, local_user.hashed_password)
       end
 
       def user_change_password(username, password)
-        user = user_repo.find_by_name(username)
-        return false if user.nil?
+        local_user = local_user_repo.find_by_name(username)
+        return false if local_user.nil?
 
         hashed_password = hash_password(password)
-        user_repo.update(user.id, hashed_password:)
+        local_user_repo.update(local_user.id, hashed_password:)
         true
       end
 
       def user_lock(username)
-        user = user_repo.find_by_name(username)
-        return false if user.nil?
-        return false if user.locked
+        local_user = local_user_repo.find_by_name(username)
+        return false if local_user.nil?
+        return false if local_user.locked
 
-        user_repo.update(user.id, locked: true)
+        local_user_repo.update(local_user.id, locked: true)
         true
       end
 
       def user_unlock(username, _password = nil)
-        user = user_repo.find_by_name(username)
-        return false if user.nil?
-        return false unless user.locked
+        local_user = local_user_repo.find_by_name(username)
+        return false if local_user.nil?
+        return false unless local_user.locked
 
-        user_repo.update(user.id, locked: false)
+        local_user_repo.update(local_user.id, locked: false)
         true
       end
 
       def user_list
-        user_repo.names
+        local_user_repo.names
       end
 
       def user_search(query)
-        user_repo.search_names(query)
+        local_user_repo.search_names(query)
       end
 
       def group_create(groupname, groupdata)
-        return if group_repo.exist_by_name?(groupname)
+        return if local_group_repo.exist_by_name?(groupname)
 
         params = {
           name: groupname,
           label: groupdata.label,
           attrs: groupdata.attrs || {},
         }
-        group_struct_to_data(group_repo.create(**params))
+        group_struct_to_data(local_group_repo.create(**params))
       end
 
       def group_read(groupname)
-        group_struct_to_data(group_repo.find_by_name(groupname))
+        group_struct_to_data(local_group_repo.find_by_name(groupname))
       end
 
       def group_update(groupname, groupdata)
-        group = group_repo.find_by_name(groupname)
-        return if group.nil?
+        local_group = local_group_repo.find_by_name(groupname)
+        return if local_group.nil?
 
         params = {
           label: groupdata.label,
-          **{attrs: groupdata.attrs&.merge(group.attrs)}.comact,
+          **{attrs: groupdata.attrs&.merge(local_group.attrs)}.comact,
         }
-        group_struct_to_data(group_repo.update(group.id, **params))
+        group_struct_to_data(local_group_repo.update(local_group.id, **params))
       end
 
       def group_delete(groupname)
-        group_struct_to_data(group_repo.delete_by_name(groupname))
+        group_struct_to_data(local_group_repo.delete_by_name(groupname))
       end
 
       def group_list
-        group_repo.names
+        local_group_repo.names
       end
 
       def group_search(query)
-        group_repo.search_names(query)
+        local_group_repo.search_names(query)
       end
 
       def member_list(groupname)
-        group = group_repo.find_with_users_by_name(groupname)
-        return if group.nil?
+        local_group = local_group_repo.find_with_users_by_name(groupname)
+        return if local_group.nil?
 
-        [group.lcoal_users + group.local_member_users].map(:name)
+        [local_group.lcoal_users + local_group.local_member_users].map(:name)
       end
 
       def member_add(groupname, username)
-        group = group_repo.find_by_name(groupname)
-        return false if group.nil?
+        local_group = local_group_repo.find_by_name(groupname)
+        return false if local_group.nil?
 
-        user = user_repo.find_by_name(username)
-        return false if user.nil?
-        return false if user.lcoal_group_id == group.id
+        local_user = local_user_repo.find_by_name(username)
+        return false if local_user.nil?
+        return false if local_user.lcoal_group_id == local_group.id
 
-        member = member_repo.find_by_local_user_by_local_group(user, group)
-        return false if member
+        local_member = local_member_repo.find_by_local_user_by_local_group(local_user, local_group)
+        return false if local_member
 
-        member_repo.create(local_user_id: user.id, local_group_id: group.id)
+        local_member_repo.create(local_user_id: local_user.id, local_group_id: local_group.id)
         true
       end
 
       def member_remove(groupname, username)
-        group = group_repo.find_by_name(groupname)
-        return false if group.nil?
+        local_group = local_group_repo.find_by_name(groupname)
+        return false if local_group.nil?
 
-        user = user_repo.find_by_name(username)
-        return false if user.nil?
+        local_user = local_user_repo.find_by_name(username)
+        return false if local_user.nil?
 
         change = false
 
-        if user.lcoal_group_id == group.id
-          user_repo.update(user.id, local_group_id: nil)
+        if local_user.lcoal_group_id == local_group.id
+          local_user_repo.update(local_user.id, local_group_id: nil)
           change = true
         end
 
-        member = member_repo.find_by_local_user_by_local_group(user, group)
-        if member
-          member_repo.delete(member.id)
+        local_member = local_member_repo.find_by_local_user_by_local_group(local_user, local_group)
+        if local_member
+          local_member_repo.delete(local_member.id)
           change = true
         end
         change
       end
 
       # private methods
-      private def user_repo = @container["repos.local_user_repo"]
-      private def group_repo = @container["repos.local_group_repo"]
-      private def member_repo = @container["repos.local_member_repo"]
+      private def local_user_repo = @container["repos.local_user_repo"]
+      private def local_group_repo = @container["repos.local_group_repo"]
+      private def local_member_repo = @container["repos.local_member_repo"]
 
       private def hash_password(password)
         return if password.nil?
@@ -228,24 +228,24 @@ module Yuzakan
           .call(password, hashed_password).value_or(false)
       end
 
-      private def user_struct_to_data(user)
-        return if user.nil?
+      private def user_struct_to_data(local_user)
+        return if local_user.nil?
 
         UserData.new(
-          name: user.name,
-          primary_group: user.local_group&.name,
-          groups: user.local_member_groups.map(&:name),
-          label: user.label,
-          email: user.email,
-          locked: user.locked)
+          name: local_user.name,
+          primary_group: local_user.local_group&.name,
+          groups: local_user.local_member_groups.map(&:name),
+          label: local_user.label,
+          email: local_user.email,
+          locked: local_user.locked)
       end
 
-      private def group_struct_to_data(group)
-        return if group.nil?
+      private def group_struct_to_data(local_group)
+        return if local_group.nil?
 
         GroupData.new(
-          name: group.name,
-          label: group.label)
+          name: local_group.name,
+          label: local_group.label)
       end
     end
   end
