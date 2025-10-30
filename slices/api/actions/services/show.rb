7b4221736e7fd2a74f7033fbc17eb87eaa5bc0cc
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-10-30 11:14:53 +0900

    services#showが完成

--- slices/api/actions/services/show.rb
+++ slices/api/actions/services/show.rb
@@ -4,29 +4,19 @@ module API
   module Actions
     module Services
       class Show < API::Action
-        def handle(request, response) # rubocop:disable Lint/UnusedMethodArgument
-          private def set_service
-            unless params.valid?
-              halt_json 400,
-                errors: [only_first_errors(params.errors)]
-            end
+        include Deps["repos.service_repo"]
 
-            @name = params[:id]
-            load_service
-
-            halt_json 404 if @service.nil?
-          end
-
-          self.status = 200
-          self.body = service_json
+        params do
+          required(:id) { filled(:name, max_size?: 255) }
         end
 
-        private def load_service
-          @service = @service_repository.find_with_params_by_name(@name)
-        end
+        def handle(request, response)
+          check_params(request, response)
 
-        private def service_json
-          generate_json(@service, assoc: current_level >= 5)
+          id = take_exist_id(request, response, service_repo)
+          service = service_repo.get(id)
+
+          response[:service] = service
         end
       end
     end
