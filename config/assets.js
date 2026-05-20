00e6c2c3032158c911f1a5457128e480be8fe07c
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2026-05-20 16:00:50 +0900

    aliasを設定したがうまくいかない

--- config/assets.js
+++ config/assets.js
@@ -1,8 +1,8 @@
 import * as assets from "hanami-assets";
 
-import civetPlugin from '@danielx/civet/esbuild';
-import { sassPlugin } from 'esbuild-sass-plugin';
-import { YAMLPlugin } from 'esbuild-yaml';
+import civetPlugin from "@danielx/civet/esbuild";
+import { sassPlugin } from "esbuild-sass-plugin";
+import { YAMLPlugin } from "esbuild-yaml";
 
 // Assets are managed by esbuild (https://esbuild.github.io), and can be
 // customized below.
@@ -16,19 +16,24 @@ await assets.run({
     // Use the `args.watch` boolean as a condition to apply diffierent options
     // when running `hanami assets watch` vs `hanami assets compile`.
 
-    esbuildOptions.format = 'esm';
+    esbuildOptions.format = "esm";
     esbuildOptions.target = [
-      'edge128', // for Extended Stable
-      'chrome126', // for ChromeOS LTS 126
-      'firefox128', // for ESR
-      'safari16', // for iOS 16
+      "edge128", // for Extended Stable
+      "chrome126", // for ChromeOS LTS 126
+      "firefox128", // for ESR
+      "safari16", // for iOS 16
     ];
     esbuildOptions.plugins ??= [];
     esbuildOptions.plugins.push(civetPlugin());
     esbuildOptions.plugins.push(sassPlugin({
-      silenceDeprecations: ['color-functions', 'global-builtin', 'import'],
+      silenceDeprecations: ["color-functions", "global-builtin", "import"],
     }));
     esbuildOptions.plugins.push(YAMLPlugin());
+    esbuildOptions.alias = {
+      "~": "./app/assets/js",
+      "~admin": "./slices/admin/assets/js",
+      "~api": "./slices/api/assets/js",
+    };
 
     return esbuildOptions;
   },
