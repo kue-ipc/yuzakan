79155c512a3b24885cf148e2ac141e708a590b8d
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2026-06-08 15:01:47 +0900

    indexactionを共通化

--- slices/api/actions/groups/index.rb
+++ slices/api/actions/groups/index.rb
@@ -3,28 +3,12 @@
 module API
   module Actions
     module Groups
-      class Index < API::Action
+      class Index < API::IndexAction
         include Deps["repos.group_repo"]
 
         security_level 2
 
         params do
-          optional(:page).value(:integer, gteq?: 1)
-          optional(:per_page).value(:integer, gteq?: 1, lteq?: MAX_PER_PAGE)
-
-          optional(:order).filled(:str?, included_in?: %w[
-            name
-            label
-          ].flat_map { |name| [name, "#{name}.asc", "#{name}.desc"] })
-
-          optional(:search).maybe(:str?, max_size?: 255)
-          optional(:match).filled(:str?, included_in?: %w[
-            extract
-            partial
-            forward
-            backward
-          ])
-
           optional(:primary_only).filled(:bool?)
           optional(:hide_prohibited).filled(:bool?)
           optional(:show_deleted).filled(:bool?)
@@ -43,30 +27,6 @@ module API
 
           response[:groups] = groups
           response[:pager] = pager
-        end
-
-        private def order_from_params(params)
-          return nil unless params[:order]
-
-          name, asc_desc = params[:order].split(".", 2).map(&:intern)
-          {name => asc_desc || :asc}
-        end
-
-        private def query_from_params(params)
-          return nil if params[:search].nil? || params[:search].empty?
-
-          search = params[:search].gsub("*", "%").gsub("?", "_")
-
-          case params[:match].intern
-          in :extract
-            search
-          in :forward
-            "#{search}%"
-          in :backward
-            "%#{search}"
-          in :partial | nil
-            "%#{search}%"
-          end
         end
 
         private def filter_from_params(params)
---
+++ slices/api/index_action.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module API
+  class IndexAction < API::Action
+    MAX_PER_PAGE = 1000
+
+    ORDER_PATTERNS = ["name", "label"].flat_map { |name| [name, "#{name}.asc", "#{name}.desc"] }.freeze
+
+    MATCH_PATTERNS = [
+      "extract",
+      "partial",
+      "forward",
+      "backward",
+    ].freeze
+
+    params do
+      optional(:page).value(:integer, gteq?: 1)
+      optional(:per_page).value(:integer, gteq?: 1, lteq?: MAX_PER_PAGE)
+      optional(:order).filled(:str?, included_in?: ORDER_PATTERNS)
+      optional(:search).maybe(:str?, max_size?: 255)
+      optional(:match).filled(:str?, included_in?: MATCH_PATTERNS)
+    end
+
+    private def order_from_params(params)
+      return nil unless params[:order]
+
+      name, asc_desc = params[:order].split(".", 2).map(&:intern)
+      {name => asc_desc || :asc}
+    end
+
+    private def query_from_params(params)
+      return nil if params[:search].nil? || params[:search].empty?
+
+      search = params[:search].gsub("*", "%").gsub("?", "_")
+      case params[:match]
+      when "extract"
+        search
+      when "forward"
+        "#{search}%"
+      when "backward"
+        "%#{search}"
+      when nil | "partial"
+        "%#{search}%"
+      end
+    end
+
+    private def filter_from_params(_params) = nil
+  end
+end
