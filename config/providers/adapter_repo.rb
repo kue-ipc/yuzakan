74c95ce48dfe8ba438c543e8d24f5f6d5cf00118
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2026-07-02 08:35:52 +0900

    書き方変えた

--- config/providers/adapter_repo.rb
+++ config/providers/adapter_repo.rb
@@ -1,21 +1,19 @@
 # frozen_string_literal: true
 
 Hanami.app.register_provider(:adapter_repo) do
-  prepare do
-    register "adapter_repo", Yuzakan::AdapterRepo.new
-  end
-
   start do
-    target["adapter_repo"].set!("local", class: Yuzakan::Adapters::Local)
-    target["adapter_repo"].set!("ldap", class: Yuzakan::Adapters::Ldap)
-    target["adapter_repo"].set!("ad", class: Yuzakan::Adapters::AD)
-    target["adapter_repo"].set!("posix_ldap", class: Yuzakan::Adapters::PosixLdap)
-    target["adapter_repo"].set!("samba_ldap", class: Yuzakan::Adapters::SambaLdap)
-    target["adapter_repo"].set!("google", class: Yuzakan::Adapters::Google)
-    # target["adapter_repo"].set!("microsoft", class: Yuzakan::Adapters::Microsoft) # not yet implemented
-    target["adapter_repo"].set!("test", class: Yuzakan::Adapters::Test) if Hanami.env?(:development, :test)
-    target["adapter_repo"].set!("dummy", class: Yuzakan::Adapters::Dummy) if Hanami.env?(:test)
-    target["adapter_repo"].set!("mock", class: Yuzakan::Adapters::Mock) if Hanami.env?(:test)
+    adapter_repo = Yuzakan::AdapterRepo.new
+
+    adapter_repo.set!("local", class: Yuzakan::Adapters::Local)
+    adapter_repo.set!("ldap", class: Yuzakan::Adapters::Ldap)
+    adapter_repo.set!("ad", class: Yuzakan::Adapters::AD)
+    adapter_repo.set!("posix_ldap", class: Yuzakan::Adapters::PosixLdap)
+    adapter_repo.set!("samba_ldap", class: Yuzakan::Adapters::SambaLdap)
+    adapter_repo.set!("google", class: Yuzakan::Adapters::Google)
+    # adapter_repo.set!("microsoft", class: Yuzakan::Adapters::Microsoft) # not yet implemented
+    adapter_repo.set!("test", class: Yuzakan::Adapters::Test) if Hanami.env?(:development, :test)
+    adapter_repo.set!("dummy", class: Yuzakan::Adapters::Dummy) if Hanami.env?(:test)
+    adapter_repo.set!("mock", class: Yuzakan::Adapters::Mock) if Hanami.env?(:test)
 
     vender_adapter_path = Hanami.app_path.dirname.parent / "vendor" / "adapters"
     if vender_adapter_path.directory?
@@ -32,12 +30,10 @@ Hanami.app.register_provider(:adapter_repo) do
             &.const_get(class_name)
         raise "Adapter class #{class_name} not found in #{child}" if klass.nil?
 
-        target["adapter_repo"].set!(prefix + name, class: klass)
+        adapter_repo.set!(prefix + name, class: klass)
       end
     end
-  end
 
-  stop do
-    target["adapter_repo"].clear
+    register "adapter_repo", adapter_repo
   end
 end
